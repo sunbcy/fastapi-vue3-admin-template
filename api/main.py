f6768107ee4e157ebf3124728a9ca484cda0233f
bcy <626813534@qq.com>
2025-07-09 04:18:10 +0800

    优化main.py+./mac_start.sh中的项目启动方式

--- api/main.py
+++ api/main.py
@@ -1,6 +1,11 @@
 import warnings
+import os
 
 from app import create_app
+from utils import get_os_type
+import yaml
+import subprocess
+import webbrowser
 # from app.azquotes.routes import client
 from fastapi.responses import FileResponse
 
@@ -17,7 +22,29 @@ async def index():
 # async def on_shutdown():
 #     await client.close_session()
 
+PROJ_PATH = os.path.abspath('..')
+with open(os.path.join(PROJ_PATH, 'config.yaml'), 'r') as file:
+    net_addr = yaml.safe_load(file)
+
+os_type = get_os_type()
+
+
 # 启动应用
 if __name__ == "__main__":
+    if os_type == 'Windows' or os_type == 'MacOS':
+        if net_addr.get('server').get('run_mode') == 1:  # ngrok外网发布地址
+            webbrowser.open(f"{net_addr.get('server').get('ngrok_pub_addr')}")
+            # 研究自动弹起无跳转页面的方式没意义,因为每个被转发的人都会显示.
+            # webbrowser.open(f"http://{net_addr.get('server').get('host')}:{net_addr.get('server').get('port')}/pub")
+        else:
+            webbrowser.open(f"http://{net_addr.get('server').get('host')}:{net_addr.get('server').get('port')}")
+    elif os_type == 'Android':
+        if net_addr.get('server').get('run_mode') == 1:  # ngrok外网发布地址
+            subprocess.run(['am', 'start', '-a', 'android.intent.action.VIEW',
+                            '-d', f"{net_addr.get('server').get('ngrok_pub_addr')}"])
+        else:
+            subprocess.run(['am', 'start', '-a', 'android.intent.action.VIEW',
+                            '-d', f"http://{net_addr.get('server').get('host')}:{net_addr.get('server').get('port')}"])
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5055)  # test  test by lly
+
--- mac_start.sh
+++ mac_start.sh
@@ -2,4 +2,5 @@
 python3 init_project.py
 cd api
 source venv_mac/bin/activate
-uvicorn main:app --host 0.0.0.0 --port 5055 --reload
+#uvicorn main:app --host 0.0.0.0 --port 5055 --reload
+python3 main.py
