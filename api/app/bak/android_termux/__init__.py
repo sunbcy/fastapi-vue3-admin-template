3dd8cb89d7c9f91a0a3fe72b55e8fadcdf914b5f
bcy <626813534@qq.com>
2025-07-10 20:48:14 +0800

    修改android端启动文件api/app/__init__.py中的注册路由register_routers

--- api/app/bak/android_termux/__init__.py
+++ api/app/bak/android_termux/__init__.py
@@ -17,26 +17,32 @@ def create_app():
 
 
 def register_routers(app):  # 只有在此处注册的应用才可以自动生成数据表
-    from app.azquotes import routes as azquotes_routes
-    app.include_router(azquotes_routes.router, prefix='/api/azquotes', tags=['azquotes'])
+    from app.system import routes as system_routes
+    app.include_router(system_routes.router, prefix='/api/system', tags=['system'])
 
-    from app.system_info import routes as system_info_routes
-    app.include_router(system_info_routes.router, prefix='/api/system_info', tags=['system_info'])
+    from app.code_editor import routes as code_editor_routes
+    app.include_router(code_editor_routes.router, prefix='/api/code_editor', tags=['code_editor'])
 
-    from app.jiucaigongshe import routes as jiucaigongshe_routes
-    app.include_router(jiucaigongshe_routes.router, prefix='/api/jiucaigongshe', tags=['jiucaigongshe'])
-
-    from app.liepin import routes as liepin_routes
-    app.include_router(liepin_routes.router, prefix='/api/liepin', tags=['liepin'])
-
-    from app.netease_music import routes as netease_music_routes
-    app.include_router(netease_music_routes.router, prefix='/api/netease_music', tags=['netease_music'])
-
-    # from app.pcap_analysis import routes as pcap_analysis_routes
-    # app.include_router(pcap_analysis_routes.router, prefix='/api/pcap_analysis', tags=['pcap_analysis'])
-
-    from app.databases import routes as databases_routes
-    app.include_router(databases_routes.router, prefix='/api/databases', tags=['databases'])
-
-    from app.qiyewechat import routes as qiyewechat_routes
-    app.include_router(qiyewechat_routes.router, prefix='/api/qiyewechat', tags=['qiyewechat'])
+    # from app.azquotes import routes as azquotes_routes
+    # app.include_router(azquotes_routes.router, prefix='/api/azquotes', tags=['azquotes'])
+    #
+    # from app.system_info import routes as system_info_routes
+    # app.include_router(system_info_routes.router, prefix='/api/system_info', tags=['system_info'])
+    #
+    # from app.jiucaigongshe import routes as jiucaigongshe_routes
+    # app.include_router(jiucaigongshe_routes.router, prefix='/api/jiucaigongshe', tags=['jiucaigongshe'])
+    #
+    # from app.liepin import routes as liepin_routes
+    # app.include_router(liepin_routes.router, prefix='/api/liepin', tags=['liepin'])
+    #
+    # from app.netease_music import routes as netease_music_routes
+    # app.include_router(netease_music_routes.router, prefix='/api/netease_music', tags=['netease_music'])
+    #
+    # # from app.pcap_analysis import routes as pcap_analysis_routes
+    # # app.include_router(pcap_analysis_routes.router, prefix='/api/pcap_analysis', tags=['pcap_analysis'])
+    #
+    # from app.databases import routes as databases_routes
+    # app.include_router(databases_routes.router, prefix='/api/databases', tags=['databases'])
+    #
+    # from app.qiyewechat import routes as qiyewechat_routes
+    # app.include_router(qiyewechat_routes.router, prefix='/api/qiyewechat', tags=['qiyewechat'])
